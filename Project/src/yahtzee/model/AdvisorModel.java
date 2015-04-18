1a79ce54f80ad051c6d1e8fefe579960dc302249
dlsuWang <kennethwang17@yahoo.com>
2015-04-18 20:50:29 +0800

    Update AdvisorModel.java

--- Project/src/yahtzee/model/AdvisorModel.java
+++ Project/src/yahtzee/model/AdvisorModel.java
@@ -67,11 +67,16 @@ public class AdvisorModel {
                 
 		else if(rerollNo==2)
 		{
-                    int exponent = 5 - keepers;
-                    double prob = Math.pow(1.0/6, exponent);
-                        
-                    out += prob + " ";
-                    out += rerolling;
+                    if(rerolls == 1)
+                        out += "16.6667%. ";
+                    else if(rerolls == 2)
+                        out += "30.5556%. ";
+                    else if(rerolls == 3)
+                        out += "%. ";
+                    else if(rerolls == 4)
+                        out += "%. ";
+                   
+                   out += rerolling;
                     
                 }
                 
@@ -145,10 +150,15 @@ public class AdvisorModel {
                 
 		else if(rerollNo==2)
 		{
-                    int exponent = 4 - keepers;
-                    double prob = Math.pow(1.0/6, exponent);
-                        
-                    out += prob + " ";
+                    if(rerolls == 1)
+                        out += "16.6667%. ";
+                    else if(rerolls == 2)
+                        out += "30.5556%. ";
+                    else if(rerolls == 3)
+                        out += "%. ";
+                    else if(rerolls == 4)
+                        out += "%. ";
+                                        
                     out += rerolling;
                     
                 }
@@ -156,65 +166,39 @@ public class AdvisorModel {
                 return out;
 	}
 	
-    public static String calculateYahtzeeProbability(DiceSet diceSet, int rerollNo){
+        public static String calculateYahtzeeProbability(DiceSet diceSet, int rerollNo){
         String out = "The probability of rolling a Yahtzee is ";
-        if(rerollNo==1){
-            int[] dist = diceSet.getDistribution();
-            int max = 0;
-            int valueOfMaxDist = 0;
-            for(int i=0; i<6; i++){
-                if(dist[i]>max){
-                    max = dist[i];
-                    valueOfMaxDist = i+1;
-                }
+        int[] dist = diceSet.getDistribution();
+        int max = 0;
+        int valueOfMaxDist = 0;
+        for(int i=0; i<6; i++){
+            if(dist[i]>max){
+                max = dist[i];
+                valueOfMaxDist = i+1;
             }
-            if(max==5) return "You rolled a Yahtzee!";
+        }
+        if(max==5) return "You rolled a Yahtzee!";
+        if(rerollNo==1){
             if(max==1) return "The probability of rolling a Yahtzee is 0.8717%, re-rolling dice 2, 3, 4, 5";
             if(max==2){
                 out += "2.8528% re-rolling dice ";
-                Iterator<Die> dice = diceSet.getDice();
-                String diceToReRoll = "";
-                for(int i=1; dice.hasNext(); i++){
-                    Die d = dice.next();
-                    if(d.getValue()!=valueOfMaxDist) diceToReRoll += i+", ";
-                }
-                diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
-                out += diceToReRoll;
             }
             if(max==3){
                 out += "9.3364% re-rolling dice ";
-                Iterator<Die> dice = diceSet.getDice();
-                String diceToReRoll = "";
-                for(int i=1; dice.hasNext(); i++){
-                    Die d = dice.next();
-                    if(d.getValue()!=valueOfMaxDist) diceToReRoll += i+", ";
-                }
-                diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
-                out += diceToReRoll;
             }
             if(max==4){
                 out += "30.5554% re-rolling dice ";
-                Iterator<Die> dice = diceSet.getDice();
-                String diceToReRoll = "";
-                for(int i=1; dice.hasNext(); i++){
-                    Die d = dice.next();
-                    if(d.getValue()!=valueOfMaxDist) diceToReRoll += i+", ";
-                }
-                diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
-                out += diceToReRoll;
             }
+            Iterator<Die> dice = diceSet.getDice();
+            String diceToReRoll = "";
+            for(int i=1; dice.hasNext(); i++){
+                Die d = dice.next();
+                if(d.getValue()!=valueOfMaxDist) diceToReRoll += i+", ";
+            }
+            diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
+            out += diceToReRoll;
         }
         if(rerollNo==2){
-            int[] dist = diceSet.getDistribution();
-            int max = 0;
-            int valueOfMaxDist = 0;
-            for(int i=0; i<6; i++){
-                if(dist[i]>max){
-                    max = dist[i];
-                    valueOfMaxDist = i+1;
-                }
-            }
-            if(max==5) return "You rolled a Yahtzee!";
             Iterator<Die> dice = diceSet.getDice();
             String diceToReRoll = "";
             for(int i=1; dice.hasNext(); i++){
@@ -230,8 +214,9 @@ public class AdvisorModel {
         }
         return out;
     }
+
     
-    public static String calculateFullHouseProbability(DiceSet diceSet){
+    public static String calculateFullHouseProbability(DiceSet diceSet, int rerollNo){
         String out = "The probability of rolling a Full House is ";
         int[] dist = diceSet.getDistribution();
         int max = 0;
@@ -247,20 +232,79 @@ public class AdvisorModel {
             if(dist[i]==3) hasThree++;
         }
         if(hasTwo==1 && hasThree==1) return "You rolled a Full House!";
-        if(hasTwo==2){
+        if(rerollNo==1){
+            if(hasTwo==2){
+                Iterator<Die> dice = diceSet.getDice();
+                String diceToReRoll = "";
+                for(int i=1; dice.hasNext(); i++){
+                    Die d = dice.next();
+                    if(dist[d.getValue()-1]==1) diceToReRoll += i+", ";
+                }
+                diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
+                return out+"55.5556%, re-rolling die "+diceToReRoll;
+            }
+            else if(max==1){
+                out += "7.5482%, re-rolling dice 2, 3, 4, 5";
+                return out;
+            } else if(max==2){
+                out += "14.4676%, re-rolling dice ";
+            } else if(max==3){
+                out += "13.8889%, re-rolling dice ";
+            } else{
+                boolean extra = false;
+                out += "25.8488%, re-rolling dice ";
+                Iterator<Die> dice = diceSet.getDice();
+                String diceToReRoll = "";
+                for(int i=1; dice.hasNext(); i++){
+                    Die d = dice.next();
+                    if(d.getValue()!=valueOfMaxDist) diceToReRoll += i+", ";
+                    else if(!extra){ diceToReRoll += i+", "; extra = true; }
+                }
+                diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
+                out += diceToReRoll;
+                return out;
+            }
             Iterator<Die> dice = diceSet.getDice();
             String diceToReRoll = "";
             for(int i=1; dice.hasNext(); i++){
                 Die d = dice.next();
-                if(dist[d.getValue()-1]==1) diceToReRoll += i+", ";
+                if(d.getValue()!=valueOfMaxDist) diceToReRoll += i+", ";
             }
             diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
-            return out+"33.33%, re-rolling die "+diceToReRoll;
+            out += diceToReRoll;
         }
-        else if(max==1){
-            out += "2.3148%, re-rolling dice 2, 3, 4, 5";
-        } else if(max==2){
-            out += "9.2593%, re-rolling dice ";
+        if(rerollNo==2){
+            if(hasTwo==2){
+                Iterator<Die> dice = diceSet.getDice();
+                String diceToReRoll = "";
+                for(int i=1; dice.hasNext(); i++){
+                    Die d = dice.next();
+                    if(dist[d.getValue()-1]==1) diceToReRoll += i+", ";
+                }
+                diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
+                return out+"33.3333%, re-rolling die "+diceToReRoll;
+            }
+            else if(max==1){
+                out += "3.8580%, re-rolling dice 2, 3, 4, 5";
+                return out;
+            } else if(max==2){
+                out += "9.2593%, re-rolling dice ";
+            } else if(max==3){
+                out += "13.8889%, re-rolling dice ";
+            } else{
+                boolean extra = false;
+                out += "13.8889%, re-rolling dice ";
+                Iterator<Die> dice = diceSet.getDice();
+                String diceToReRoll = "";
+                for(int i=1; dice.hasNext(); i++){
+                    Die d = dice.next();
+                    if(d.getValue()!=valueOfMaxDist) diceToReRoll += i+", ";
+                    else if(!extra){ diceToReRoll += i+", "; extra = true; }
+                }
+                diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
+                out += diceToReRoll;
+                return out;
+            }
             Iterator<Die> dice = diceSet.getDice();
             String diceToReRoll = "";
             for(int i=1; dice.hasNext(); i++){
@@ -269,37 +313,25 @@ public class AdvisorModel {
             }
             diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
             out += diceToReRoll;
-        } else if(max==3){
-            out += "13.8889%, re-rolling dice ";
-            Iterator<Die> dice = diceSet.getDice();
-            String diceToReRoll = "";
-            for(int i=1; dice.hasNext(); i++){
-                Die d = dice.next();
-                if(d.getValue()!=valueOfMaxDist) diceToReRoll += i+", ";
-            }
-            diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
-            out += diceToReRoll;
-        } else{
-            out += "why bother, you shouldn't even be aiming for a Full House right now :P";
         }
         return out;
     }
     
     public static String calculateThreeOfAKindProbability(DiceSet diceSet, int rerollNo){
         String out = "The probability of rolling Three of A Kind is ";
-        if(rerollNo==1){
-            int[] dist = diceSet.getDistribution();
-            int max = 0;
-            int valueOfMaxDist = 0;
-            for(int i=0; i<6; i++){
-                if(dist[i]>max){
-                    max = dist[i];
-                    valueOfMaxDist = i+1;
-                }
+        int[] dist = diceSet.getDistribution();
+        int max = 0;
+        int valueOfMaxDist = 0;
+        for(int i=0; i<6; i++){
+            if(dist[i]>max){
+                max = dist[i];
+                valueOfMaxDist = i+1;
             }
-            if(max>=3){
-                return "You rolled Three of A Kind!";
-            } 
+        }
+        if(max>=3){
+            return "You rolled Three of A Kind!";
+        } 
+        if(rerollNo==1){
             if(max==1) return "The probability of rolling Three of A Kind is 35.8112%, re-rolling dice 2, 3, 4, 5";
             if(max==2){
                 out+="66.5102%, re-rolling dice ";
@@ -314,18 +346,7 @@ public class AdvisorModel {
             }
         }
         if(rerollNo==2){
-            int[] dist = diceSet.getDistribution();
-            int max = 0;
-            int valueOfMaxDist = 0;
-            for(int i=0; i<6; i++){
-                if(dist[i]>max){
-                    max = dist[i];
-                    valueOfMaxDist = i+1;
-                }
-            }
-            if(max>=3){
-                return "You rolled Three of A Kind!";
-            } else if(max==1){
+            if(max==1){
                 out+="13.1944%, re-rolling dice 2, 3, 4, 5";
             } else if(max==2){
                 out+="42.1296%, re-rolling dice ";
@@ -343,23 +364,28 @@ public class AdvisorModel {
         
     }
     
-    public static String calculateFourOfAKindProbability(DiceSet diceSet){
+    public static String calculateFourOfAKindProbability(DiceSet diceSet, int rerollNo){
         String out = "The probability of rolling Four of A Kind is ";
-        int[] dist = diceSet.getDistribution();
-        int max = 0;
-        int valueOfMaxDist = 0;
-        for(int i=0; i<6; i++){
-            if(dist[i]>max){
-                max = dist[i];
-                valueOfMaxDist = i+1;
+        if(rerollNo==1){
+            int[] dist = diceSet.getDistribution();
+            int max = 0;
+            int valueOfMaxDist = 0;
+            for(int i=0; i<6; i++){
+                if(dist[i]>max){
+                    max = dist[i];
+                    valueOfMaxDist = i+1;
+                }
             }
-        }
-        if(max>=4){
-            return "You rolled Four of A Kind!";
-        } else if(max==1){
-            out+="1.6204%, re-rolling dice 2, 3, 4, 5";
-        } else if(max==2){
-            out+="7.4074%, re-rolling dice ";
+            if(max>=4){
+                return "You rolled Four of A Kind!";
+            } 
+            if(max==1) return "The probability of rolling Four of A Kind is 8.7961%, re-rolling dice 2, 3, 4, 5";
+            if(max==2){
+                out+="22.3037%, re-rolling dice ";
+            }
+            if(max==3){
+                out+="51.7747%, re-rolling dice ";
+            }
             Iterator<Die> dice = diceSet.getDice();
             String diceToReRoll = "";
             for(int i=1; dice.hasNext(); i++){
@@ -368,8 +394,27 @@ public class AdvisorModel {
             }
             diceToReRoll = diceToReRoll.substring(0, diceToReRoll.length()-2);
             out += diceToReRoll;
-        } else if(max==3){
-            out+="30.5556%, re-rolling dice ";
+        }
+        if(rerollNo==2){
+            int[] dist = diceSet.getDistribution();
+            int max = 0;
+            int valueOfMaxDist = 0;
+            for(int i=0; i<6; i++){
+                if(dist[i]>max){
+                    max = dist[i];
+                    valueOfMaxDist = i+1;
+                }
+            }
+            if(max>=4){
+                return "You rolled Four of A Kind!";
+            } else if(max==1){
+                out+="1.6204%, re-rolling dice 2, 3, 4, 5";
+                return out;
+            } else if(max==2){
+                out+="7.4074%, re-rolling dice ";
+            } else if(max==3){
+                out+="30.5556%, re-rolling dice ";
+            }
             Iterator<Die> dice = diceSet.getDice();
             String diceToReRoll = "";
             for(int i=1; dice.hasNext(); i++){
@@ -381,7 +426,7 @@ public class AdvisorModel {
         }
         return out;
     }
-    
+        
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         while(true){
@@ -396,10 +441,12 @@ public class AdvisorModel {
             System.out.println("reroll #2 " + calculateSmallStraightProbability(ds,2));
             System.out.println(calculateYahtzeeProbability(ds, 1));
             System.out.println(calculateYahtzeeProbability(ds, 2));
-            System.out.println(calculateFullHouseProbability(ds));
+            System.out.println(calculateFullHouseProbability(ds, 1));
+            System.out.println(calculateFullHouseProbability(ds, 2));
             System.out.println(calculateThreeOfAKindProbability(ds, 1));
             System.out.println(calculateThreeOfAKindProbability(ds, 2));
-            System.out.println(calculateFourOfAKindProbability(ds));
+            System.out.println(calculateFourOfAKindProbability(ds, 1));
+            System.out.println(calculateFourOfAKindProbability(ds, 2));
             System.out.println("");
         }
     }
